c5f4acf35ef64b473188e206e0af3ba65a1398ca
Leonardo Martinez Vega <78712409+LeonardoMartinezVega@users.noreply.github.com>
2021-09-29 22:55:25 -0500

    added comments to explain player movement

--- Iddy and Mego/Assets/PlayerMovement.cs
+++ Iddy and Mego/Assets/PlayerMovement.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour{
 
-    private Rigidbody2D rb;
-    private BoxCollider2D cl;
-    private float speed = 1f;
-    private static float max = 14f;
+    private Rigidbody2D rb; // player
+    private BoxCollider2D cl; //terrain
+    private float speed = 1f; // base speed
+    private static float max = 14f; // max speed.
 
     [SerializeField] private LayerMask ground;
 
@@ -23,30 +23,30 @@ public class PlayerMovement : MonoBehaviour{
 
         float dirX = Input.GetAxisRaw("Horizontal");
         float dirY = Input.GetAxisRaw("Jump");
- 
+        // takes whatever button that affects horizontal movement(currently a and d)
         if(Input.GetButton("Horizontal")){
 
-            Debug.Log(speed);
-            Debug.Log(dirY);
-            rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
-
+            rb.velocity = new Vector2(dirX * speed, rb.velocity.y); // moves the player horizontally
+            // the longer the button is held the faster movement becomes until it reaches the max
             if (speed < max){
+                // rate at which speed is gained. subject to change
                 speed += .01f;
             }
        }
-
+       // when switching directions, or stopping motion, player loses momentum and returns to base speed 
        if (Input.GetButtonUp("Horizontal")){
 
             if (speed > 1f){
                 speed = 1f;
             }
         }
-       
+       // checks if player is on the ground  to jump
         if (Input.GetButtonDown("Jump") && onGround()){
-
-            rb.velocity = new Vector2(rb.velocity.x, (dirY * (speed / 2)) + 12f);
+            //speed affects jump height but not as much as horizontal speed. subject to change
+            rb.velocity = new Vector2(rb.velocity.x, (dirY * (speed / 2)) + 12f); 
         }
     }
+    // checks if player is on the ground
     private bool onGround(){
 
         return Physics2D.BoxCast(cl.bounds.center, cl.bounds.size, 0f, Vector2.down, .1f, ground);
